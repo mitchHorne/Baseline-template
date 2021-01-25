94c6a2d7285cdb8602fb640e1a7bc37984969572
Mitch <mitchhorne92@gmail.com>
2021-01-25 17:54:37 +0200

    Store example of creating a form for future automation

---
+++ src/functions/createForm.js
@@ -0,0 +1,30 @@
+// Example of creating a form
+
+// const validate = values => {
+//   const errors = {};
+//   if (!values.name) errors.name = "Required";
+//   if (!values.surname) errors.surname = "Required";
+//   if (!values.price) errors.price = "Required";
+
+//   return errors;
+// };
+
+// const FormComponents = () => (
+//     <Fragment>
+//       <TextField name="name" label="Name" />
+//       <TextField name="surname" label="Surname" />
+//       <TextField name="price" label="Price" type="number" />
+//       <Checkbox name="sauces" label="Ketchup" />
+//       <Checkbox name="sauces" label="Mustard" />
+//       <Checkbox name="sauces" label="BBQ" />
+//       <Checkbox name="sauces" label="Mayo" />
+//       <Button type="submit">Submit</Button>
+//     </Fragment>
+//   );
+
+{
+  /* <Form
+  FormComponents={FormComponents}
+  onSubmit={values => console.log(values)}
+/>; */
+}
