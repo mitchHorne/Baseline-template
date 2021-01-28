586feddcced251f99af76d2314b20b081ab1eebb
Mitch <mitchhorne92@gmail.com>
2021-01-28 18:25:53 +0200

    Add banner example

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,19 +1,5 @@
 import { Fragment } from "react";
 
-import Banner from "../components/Banner";
-
-import bannerSrc from "../constants/banner.jpg";
-
-const coverText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-const coverButtons = [
-  { href: "#", text: "Get Quote" },
-  { href: "#", text: "Get Other" },
-];
-
-const Main = () => (
-  <Fragment>
-    <Banner buttons={coverButtons} src={bannerSrc} text={coverText} />
-  </Fragment>
-);
+const Main = () => <Fragment></Fragment>;
 
 export default Main;
---
+++ src/functions/createBanner.js
@@ -0,0 +1,9 @@
+// Example of creating a banner
+
+// const coverText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+// const coverButtons = [
+//   { href: "#", text: "Get Quote" },
+//   { href: "#", text: "Get Other" },
+// ];
+
+// <Banner buttons={coverButtons} src={bannerSrc} text={coverText} />
